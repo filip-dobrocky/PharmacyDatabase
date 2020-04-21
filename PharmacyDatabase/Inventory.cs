78e5cfc4bf95976e4e4c8583d9b63addc19311ca
Filip Dobrocký <xdobro22@vutbr.cz>
2020-04-21 21:40:29 +0200

    Added inventory

---
+++ PharmacyDatabase/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyDatabase
+{
+    public class Inventory
+    {
+        public void Resupply(Product product, Supplier supplier, int Amount)
+        {
+            //TODO: implement
+        }
+
+        public void Sell(Product product, int Amount)
+        {
+            //TODO: implement
+        }
+    }
+}
