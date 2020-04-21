f334cf3b1fa2a133560937fbf9f21eea957b633a
Filip Dobrocký <xdobro22@vutbr.cz>
2020-04-21 20:27:40 +0200

    Added basic functionality of adding/removing products and suppliers

---
+++ PharmacyDatabase/ProductList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyDatabase
+{
+    public class ProductList
+    {
+        private DataClassesDataContext _db = new DataClassesDataContext();
+
+        public IQueryable<Product> Products
+        {
+            get { return _db.Products; }
+        }
+
+        public void Add(string name, string manufacturer, bool prescription)
+        {
+            if (_db.Products.Any(x => x.Name == name))
+                throw new Exception("Product with given name already exists");
+            _db.Products.InsertOnSubmit(new Product() { Name = name,
+                                                        Manufacturer = manufacturer,
+                                                        Prescription = prescription });
+            _db.SubmitChanges();
+        }
+
+        public void Remove(Guid id)
+        {
+            _db.Products.DeleteAllOnSubmit(from s in _db.Products
+                                           where s.Id == id
+                                           select s);
+            _db.SubmitChanges();
+        }
+
+        public void Import(string filePath)
+        {
+            //TODO: implement
+        }
+
+        public void Export(string filePath)
+        {
+            //TODO: implement
+        }
+    }
+}
---
+++ PharmacyDatabase/SupplierList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyDatabase
+{
+    public class SupplierList
+    {
+        private DataClassesDataContext _db = new DataClassesDataContext();
+
+        public IQueryable<Supplier> Suppliers
+        {
+            get { return _db.Suppliers; }
+        }
+
+        public void Add(string name)
+        {
+            if (_db.Suppliers.Any(x => x.Name == name))
+                throw new Exception("Supplier with given name already exists");
+            _db.Suppliers.InsertOnSubmit(new Supplier() { Name = name });
+            _db.SubmitChanges();
+        }
+
+        public void Remove(Guid id)
+        {
+            _db.Suppliers.DeleteAllOnSubmit(from s in _db.Suppliers
+                                            where s.Id == id
+                                            select s);
+            _db.SubmitChanges();
+        }
+
+        public void Import(string filePath)
+        {
+            //TODO: implement
+        }
+
+        public void Export(string filePath)
+        {
+            //TODO: implement
+        }
+    }
+}
