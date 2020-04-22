037ed90ea7fcfd7ec2cd44bdf462bbb9b0a5b6b3
Filip Dobrocký <xdobro22@vutbr.cz>
2020-04-22 23:23:09 +0200

    Basic inventory functionality

--- PharmacyDatabase/Inventory.cs
+++ PharmacyDatabase/Inventory.cs
@@ -8,14 +8,44 @@ namespace PharmacyDatabase
 {
     public class Inventory
     {
-        public void Resupply(Product product, Supplier supplier, int Amount)
+        IEnumerable<Product> AvailableProducts
         {
-            //TODO: implement
+            //TODO: Implement
         }
 
-        public void Sell(Product product, int Amount)
+        public void Resupply(Product product, Supplier supplier, int amount)
         {
-            //TODO: implement
+            using (DataClassesDataContext db = new DataClassesDataContext())
+            {
+                if (db.InventoryProducts.Any(x => x.ProductId == product.Id))
+                {
+                    var p = db.InventoryProducts.Single(x => x.ProductId == product.Id);
+                    p.Amount += amount;
+                }
+                else
+                {
+                    db.InventoryProducts.InsertOnSubmit(new InventoryProduct() { ProductId = product.Id,
+                                                                                 Amount = amount });
+                }
+                db.SubmitChanges();
+            }
+        }
+
+        public void Sell(Product product, int amount)
+        {
+            using (DataClassesDataContext db = new DataClassesDataContext())
+            {
+                if (db.InventoryProducts.Any(x => x.ProductId == product.Id))
+                {
+                    var p = db.InventoryProducts.Single(x => x.ProductId == product.Id);
+                    p.Amount -= amount;
+                }
+                else
+                {
+                    throw new Exception("Product not available");
+                }
+                db.SubmitChanges();
+            }
         }
     }
 }
